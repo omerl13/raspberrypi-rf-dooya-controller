f40bb0ed73645d2fdd8914de2e2aef9dba21672c
Omer Levi <omerl1308@gmail.com>
2020-10-02 14:46:02 +0300

    Messages printing

--- receiver.py
+++ receiver.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     GPIO.setup(RECEIVE_PIN, GPIO.IN)
     cumulative_time = 0
     beginning_time = datetime.now()
-    print('**Started recording**')
+    print('** Started recording **')
     psec = 0
     while cumulative_time < RECORDING_DURATION:
         if cumulative_time > psec:
@@ -20,15 +20,15 @@ if __name__ == '__main__':
         signals[0].append(time_delta)
         signals[1].append(GPIO.input(RECEIVE_PIN))
         cumulative_time = time_delta.seconds
-    print('**Ended recording**')
+    print('** Ended recording **')
     GPIO.cleanup()
 
-    print('**Processing results**')
+    print('** Processing results **')
     for i in range(len(signals[0])):
         signals[0][i] = signals[0][i].seconds + \
             signals[0][i].microseconds/1000000.0
 
-    print('**Plotting results**')
+    print('** Plotting results **')
     pyplot.plot(signals[0], signals[1])
     pyplot.axis([0, RECORDING_DURATION, -1, 2])
     pyplot.show()
